7d32f9ed63551b7ba97aec9d13aa3874e02198e6
Diogo Costa <diogo.costa.t0126447@edu.atec.pt>
2025-02-20 10:56:46 +0000

    Exercicio1

--- UFCD10789/Exercicio1/exercicio1.c
+++ UFCD10789/Exercicio1/exercicio1.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-int main(){
-int Num=0;
-for(int i=0;i<10;i++){
-    Num+2;
-    printf("Num = %d, i = %d", Num,i);
-    printf("aqui1FOR");
-} 
-return 0;
+// Desenvolva um programa que assuma uma entrada em Segundos e transforme em Horas, Minutos e Segundos.
+
+int main() {
+    int segundos;
+
+    printf("Insira o tempo em segundos: ");
+    scanf("%d", &segundos);
+
+    int horas = segundos / 3600;
+    segundos %= 3600;
+    int minutos = segundos / 60;
+    segundos %= 60;
+
+    printf("O numero inserido equivale a %d Horas, %d Minutos e %d Segundos.\n",horas, minutos, segundos);
+
+    return 0;
 }
